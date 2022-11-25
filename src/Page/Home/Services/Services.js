977f596eae79ad30260b9f90a6998e0e04f3cc7c
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-11-25 15:37:00 +0600

    extra section on homepage added

--- src/Page/Home/Services/Services.js
+++ src/Page/Home/Services/Services.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import ServicesCard from "./ServicesCard";
 
 const Services = () => {
-  return <div></div>;
+  const { data: services = [], isLoading } = useQuery({
+    queryKey: ["services"],
+    queryFn: () =>
+      fetch("http://localhost:5000/services").then((res) => res.json()),
+  });
+
+  return (
+    <div className="grid grid-cols-4 gap-5 px-10 my-10">
+      {services.map((service) => (
+        <ServicesCard key={service._id} service={service}></ServicesCard>
+      ))}
+    </div>
+  );
 };
 
 export default Services;
--- src/Page/Home/Services/ServicesCard.js
+++ src/Page/Home/Services/ServicesCard.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { FaTruckMoving } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
+import { BiSupport } from "react-icons/bi";
+import { BsPatchCheckFill } from "react-icons/bs";
 
-const ServicesCard = () => {
-  return <div></div>;
+const ServicesCard = ({ service }) => {
+  const { service_name, service_desc, icon } = service;
+  return (
+    <div
+      className="card hover:shadow-xl"
+      style={{
+        backgroundImage: `url('https://i.ibb.co/1n5GxPP/white-brush-stroke-texture-background-min.jpg')`,
+        backgroundSize: "cover",
+      }}
+    >
+      <figure className="px-10 pt-10">
+        {icon === "RiTeamFill" && (
+          <RiTeamFill className="text-5xl text-blue-900"></RiTeamFill>
+        )}
+        {icon === "BiSupport" && (
+          <BiSupport className="text-5xl text-blue-900"></BiSupport>
+        )}
+        {icon === "BsPatchCheckFill" && (
+          <BsPatchCheckFill className="text-5xl text-blue-900"></BsPatchCheckFill>
+        )}
+        {icon === "FaTruckMoving" && (
+          <FaTruckMoving className="text-5xl text-blue-900"></FaTruckMoving>
+        )}
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-blue-900">{service_name}</h2>
+        <p>{service_desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ServicesCard;
