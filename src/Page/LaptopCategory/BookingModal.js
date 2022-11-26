0d60bda9881db3f3890ebf86a4c60905902476ff
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-11-26 12:14:31 +0600

    booking info sent to the database

--- src/Page/LaptopCategory/BookingModal.js
+++ src/Page/LaptopCategory/BookingModal.js
@@ -1,16 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { AuthContext } from "../../contexts/AuthProvider";
 
 const BookingModal = ({ categoryInfo }) => {
+  const [bookedInfo, setBookedInfo] = useState(false);
   const { user } = useContext(AuthContext);
-  const { laptop_name, resale_price, booked } = categoryInfo;
-  const { register, handleSubmit } = useForm();
+  const { _id, laptop_img, laptop_name, resale_price } = categoryInfo;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleBooking = (data, e) => {
     console.log(data);
+    const myOrdersInfo = {
+      buyerPhoneNumber: data.phoneNumber,
+      meetingAddress: data.meetingAddress,
+      laptop_img,
+      laptop_name,
+      resale_price,
+      payment_status: false,
+    };
+    fetch("http://localhost:5000/myOrders", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(myOrdersInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (bookedInfo) {
+          updateBooked(_id, bookedInfo);
+        }
+      })
+      .catch((e) => console.log(e));
     e.target.reset();
   };
+
+  const updateBooked = (id, booked) => {
+    const bookedDoc = {
+      booked,
+    };
+    fetch(`http://localhost:5000/booked/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(bookedDoc),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        toast.success(`${laptop_name} successfully Booked`);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleBookedInfo = () => {
+    setBookedInfo(true);
+  };
+
   return (
     <>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -40,6 +93,7 @@ const BookingModal = ({ categoryInfo }) => {
               </label>
               <input
                 type="text"
+                name="email"
                 placeholder="user email"
                 className="input input-bordered w-full max-w-lg"
                 defaultValue={user?.email}
@@ -70,27 +124,40 @@ const BookingModal = ({ categoryInfo }) => {
               </label>
               <input
                 {...register("phoneNumber", {
-                  require: "Phone number is required",
+                  required: "Phone number is required",
                 })}
                 type="text"
                 placeholder="phone number"
                 className="input input-bordered w-full max-w-lg"
               />
-
+              {errors.phoneNumber && (
+                <p className="text-red-600 mt-2">
+                  {errors.phoneNumber?.message}
+                </p>
+              )}
               <label className="label">
                 <span className="label-text">Meeting Address</span>
               </label>
               <input
                 {...register("meetingAddress", {
-                  require: "Meeting address is required",
+                  required: "Meeting address is required",
                 })}
                 type="text"
                 placeholder="meeting address"
                 className="input input-bordered w-full max-w-lg"
               />
+              {errors.meetingAddress && (
+                <p className="text-red-600 mt-2">
+                  {errors.meetingAddress?.message}
+                </p>
+              )}
             </div>
-            <button className="max-w-lg w-full btn btn-primary mt-5">
-              Submit
+            <button
+              onClick={handleBookedInfo}
+              type="submit"
+              className="max-w-lg w-full btn btn-primary mt-5"
+            >
+              <label htmlFor="booking-modal">Submit</label>
             </button>
           </form>
         </div>
